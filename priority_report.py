7e9ba249a4a0748d83a3644609c58d415cbd964a
DLBPointon <damonlbp@hotmail.co.uk>
2022-09-12 14:49:55 +0100

    Updates for much neater output

--- priority_report.py
+++ priority_report.py
@@ -3,82 +3,67 @@ import os
 import sys
 from dotenv import load_dotenv
 from datetime import date
-
+import pandas as pd
+from tabulate import tabulate
+import requests
 
 def dotloader():
     load_dotenv()
     jira_user = os.getenv('JIRA_USER')
     jira_pass = os.getenv('JIRA_PASS')
-
-    slack_add = os.getenv('JOW_HOOK')
+    slack_add = os.getenv('PRI_SLACK')
     
-    if sys.argv[1] == 'DAM':
-        slack_add = os.getenv('DAM_HOOK')
-    elif sys.argv[1] == 'CUR':
-        slack_add = os.getenv('CUR_HOOK')
-    elif sys.argv[1] == 'PRI':
-        slack_add = os.getenv('PRI_HOOK')
-    else:
-        print('Not Running, probably no argument')
     return jira_user, jira_pass, slack_add
 
-
 def authorise(user, password):
+    data_dict = {}
+
     jira = "https://grit-jira.sanger.ac.uk"
     auth_jira = JIRA(jira, basic_auth=(user, password))
     projects = auth_jira.search_issues(f'project IN ("GRIT","RC") AND status NOT IN ("Submitted", "Done", "Cancelled")',
-                                       maxResults=10000)
+                                        maxResults=10000)
     result_list = ''
     if len(projects) >= 1:
         for i in projects:
             issue = auth_jira.issue(f'{i}')
             level = issue.fields.priority
-            if str(issue).startswith('R'):
-                issuen = str(issue) + ' \t'
-            else:
-                issuen = str(issue)
-            if not str(issue.fields.status) == 'Decontamination':
-                stat = str(issue.fields.status) + '\t'
-            else:
-                stat = str(issue.fields.status)
-            if str(level) in 'High':
-                result_list += f'| {str(issue.fields.customfield_10201)} = ' \
-                               f' {issuen} = ' \
-                               f' {str(issue.fields.priority)} = ' \
-                               f' {stat} = ' \
-                               f' {str(issue.fields.assignee)} |\n'
-            elif str(level) in 'Highest':
-                result_list += f'| {str(issue.fields.customfield_10201)} = ' \
-                               f' {issuen} = ' \
-                               f' {str(issue.fields.priority)} = ' \
-                               f' {stat} = ' \
-                               f' {str(issue.fields.assignee)} |\n'
+            issuen = str(issue)
+            stat = str(issue.fields.status)
+            if str(level) in 'High' or str(level) in 'Highest':
+                key = str(issue.fields.customfield_10201)
+                data_dict[key] = {'Ticket_id':issuen,
+                                    'Priority Level': str(issue.fields.priority),
+                                    'Status': stat,
+                                    'Assignee': str(issue.fields.assignee)}
 
-    return result_list
+    return data_dict
 
+def convert_to_df(ticket_dict):
+    return pd.DataFrame.from_dict(ticket_dict).T
 
-def make_json(result_list):
+def tabulate_df(df):
+    return tabulate(df, tablefmt = "grid", headers=['Organism', 'Ticket ID', 'Priority Level', 'Status', 'Assignee'])
 
-    message_package = '{"text":"\n' + \
-                      f'|-------- Rovers Priority Report for {date.today()} --------|\n' + \
-                      f'|========================================|\n' + \
-                      f'{result_list}\n' + \
-                      f'|========================================|\n' + \
-                      f'|--------------- END Report for {date.today()} -------------|\n' + \
-                      '"}'
-    return message_package
+def post_it(p_df, token):
+    headers = {
+        'Content-Type': 'application/json',
+            }
 
+    data = '{"text":' + "'Report for: " + str(date.today().strftime('%d-%b-%Y')) + " \n ```" + str(p_df) + "```" + '}'
 
-def post_it(json, hook):
-    os.popen(f"curl -X POST -H 'Content-type: application/json' --data '{json}' {hook}").read()
-
+    res = requests.post(token, headers=headers, data=data)
+    print(res)
 
 def main():
     user, passw, slack_add = dotloader()
     message = authorise(user, passw)
-    new_message = make_json(message)
-    post_it(new_message, slack_add)
+    df = convert_to_df(message)
 
+    df['Assignee'].replace('None', 'Waiting on Assignment', inplace=True)
+    df.sort_values(['Assignee'], axis=0, inplace=True)
+
+    prettier_df = tabulate_df(df)
+    post_it(prettier_df, slack_add)
 
 if __name__ == '__main__':
     main()
